d6dc24166e08257f43373cfff461eabf1d3c9f52
faizanshabir123 <faizanshabir69@gmail.com>
2021-10-17 15:21:33 +0500

    Create Ch#23 Dictionaries.py

---
+++ Ch#23 Dictionaries.py
@@ -0,0 +1,14 @@
+# some examples of Python dictionaries:
+
+EnglishFrench = {} # empty dictionary
+EnglishFrench["book"] = "livre" # add a key-value pair to the dictionary
+EnglishFrench["pen"] = "stylo"
+
+print(EnglishFrench["book"]) # access a value in the dictionary
+print(EnglishFrench["pen"]) # access a value in the dictionary
+
+# alternative method of setting up a dictionary
+ComputingTerms = {"Boolean" : "can be TRUE or FALSE", "Bit" : "0 or 1"}
+
+print(ComputingTerms["Boolean"])
+print(ComputingTerms["Bit"])
